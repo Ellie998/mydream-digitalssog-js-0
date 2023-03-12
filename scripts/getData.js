37c8d51a3d88c9c53b9314e43f792ba5aee00dac
Ellie <dpfla9144@naver.com>
2023-03-12 13:29:19 +0900

    append child app names to category ol

--- scripts/getData.js
+++ scripts/getData.js
@@ -1,10 +1,22 @@
+const appData = [];
+const appNameElements = document.querySelectorAll(".category-wrap h3");
+
 // read local JSON file in javascript
 fetch("../data/appData.json")
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    for (const item of data) {
+      appData.push(item);
+      for (const appItem of appNameElements) {
+        if (item.category == appItem.innerText) {
+          console.log(item.name);
+          const newList = document.createElement("li");
+          newList.innerText = item.name;
+          appItem.nextElementSibling.appendChild(newList);
+        }
+      }
+    }
+    // console.dir(appData[0].name);
   });
-
-alert("hi");
